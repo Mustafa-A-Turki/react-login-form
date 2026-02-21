888cce24ba37c41aab19e78a6fee4c632b1b9596
Mostafa Ashour <mostafa.ashour.tk@gmail.com>
2026-02-22 00:23:33 +0200

    add Header to ImageSide and structure layout

---
+++ src/Components/Header.js
@@ -0,0 +1,8 @@
+export default function Header () {
+    return (
+        <div>
+            <a href="#">Website name</a>
+            <button>Back to Website {"icon"}</button>
+        </div>
+    )
+}
